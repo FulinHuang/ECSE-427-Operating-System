b612d5cef0094c271449cde16c9fc3191c18b70c
FulinHuang <fulin.huang@mail.mcgill.ca>
2020-03-06 17:33:37 -0500

    init a3

---
+++ Assignment3/cmake-build-debug/CMakeFiles/Assignmen3.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignmen3.dir/shell.c.o"
+  "CMakeFiles/Assignmen3.dir/interpreter.c.o"
+  "CMakeFiles/Assignmen3.dir/shellmemory.c.o"
+  "CMakeFiles/Assignmen3.dir/ram.c.o"
+  "CMakeFiles/Assignmen3.dir/pcb.c.o"
+  "CMakeFiles/Assignmen3.dir/kernel.c.o"
+  "CMakeFiles/Assignmen3.dir/cpu.c.o"
+  "CMakeFiles/Assignmen3.dir/memorymanager.c.o"
+  "Assignmen3.pdb"
+  "Assignmen3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignmen3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Assignment3/cmake-build-debug/CMakeFiles/Assignment2.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment2.dir/shell.c.o"
+  "CMakeFiles/Assignment2.dir/interpreter.c.o"
+  "CMakeFiles/Assignment2.dir/shellmemory.c.o"
+  "CMakeFiles/Assignment2.dir/ram.c.o"
+  "CMakeFiles/Assignment2.dir/pcb.c.o"
+  "CMakeFiles/Assignment2.dir/kernel.c.o"
+  "CMakeFiles/Assignment2.dir/cpu.c.o"
+  "Assignment2.pdb"
+  "Assignment2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
